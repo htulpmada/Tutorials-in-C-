9e7b1af0f9d3469a57f14a9bad26024f42d3785c
apluth <apluth@crimson.ua.edu>
2016-06-06 13:26:17 -0500

    controls statements enums and Gui project shell

---
+++ Switch and enum/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_enum_and_Exception_handle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum Temperature
+        {
+            Low,
+            Medium,
+            High
+        };
+        public enum Colors
+        {
+            red,blue,green,yellow
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string inVal = "green";
+            Colors newColor = (Colors)Enum.Parse(typeof(Colors), inVal);
+            //check type
+            if(newColor == Colors.green)
+            {
+                MessageBox.Show(newColor.ToString());
+            }
+            //Loop through all values in enum
+            foreach (Colors iColor in Enum.GetValues(typeof(Colors))) { MessageBox.Show(iColor.ToString());}
+            Temperature value = Temperature.Medium;
+            if(value==Temperature.Medium)
+            {
+                MessageBox.Show("Temperature is Medium.. ");
+            }
+            try
+            {
+                int val = 100;
+                int div = 0;
+                int resultVal;
+                resultVal = (val / div);
+                MessageBox.Show("The result is :" + resultVal);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Exception catch here - details : " + ex.ToString());
+            }
+            finally { MessageBox.Show("Enter Finally block "); }
+            findStatus("A+");
+        }
+        public void findStatus(string val) 
+        {
+            switch(val)
+            {
+                case "A+":
+                    MessageBox.Show("Excellent !!");
+                    break;
+                case "A":
+                    MessageBox.Show("Very Good !!");
+                    break;
+                case "B":
+                    MessageBox.Show("Good !!");
+                    break;
+                case "C":
+                    MessageBox.Show("Passed !!");
+                    break;
+                case "D":
+                    MessageBox.Show("Failed !!");
+                    break;
+                default:
+                    MessageBox.Show("Out of Range !!");
+                    break;
+            }
+        }
+    }
+}
---
+++ for_Loop/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //comparison of two ints
+            int count = 4;
+            for(int i=1;i<=count;i++)
+            {
+                MessageBox.Show("Current value of i is -"+i);
+            }
+            //set int for comparison
+            for (int i=1;i<=5;i++)
+            {
+                if (i == 2) continue;
+                if (i == 3) break;
+                MessageBox.Show("execut " + i + " times !!");
+            }
+            //for each loop
+            string[] days = { "Sunday","Monday","TuesDay","Wednsday","Thursday","Friday","Saturday"};
+            foreach(string day in days)
+            {
+                MessageBox.Show("The Day is : "+day);
+            }
+            //while
+            int cnt = 1;
+            while(cnt<=4)
+            {
+                MessageBox.Show("The value of i is : "+cnt);
+                cnt += 1;
+            }
+            //do while loop
+            int c = 4;
+            do{
+                MessageBox.Show(" Loop Exectued :"+c);
+                c++;
+            } while (c <= 4);
+
+        }
+    }
+}
