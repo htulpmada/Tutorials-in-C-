b860a277fd77a48b6b3cd3ff38c8e287b5da210f
apluth <apluth@crimson.ua.edu>
2016-06-06 12:28:32 -0500

    Windows CultureInfo -intro

---
+++ Culture/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Culture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                MessageBox.Show(ci.EnglishName+ " - " + ci.Name);
+            }
+        }
+    }
+}
